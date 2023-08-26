c063058aa903f054cf07f2051cc7596b494b3706
yu <yuprograming@gmail.com>
2023-08-27 05:50:51 +0900

    header実装

--- src/js/script.js
+++ src/js/script.js
@@ -1,5 +1,10 @@
 
 jQuery(function ($) { // この中であればWordpressでも「$」が使用可能になる
 
+// ハンバーガーメニュー
+$(".js-hamburger,.js-drawer,.drawer-nav__item").click(function () {
+  $(".js-hamburger").toggleClass("is-active");
+  $(".js-drawer").toggleClass("is-active");
+});
 
 });
